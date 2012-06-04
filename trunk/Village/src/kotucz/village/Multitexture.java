1c4af78fd79aa27ec4f06129ac9cb597930fb585
TomasRKotula@gmail.com <TomasRKotula@gmail.com@ef30e6ac-8c31-e848-2268-f4fed5c5a1f9>
2012-06-04 16:37:00 +0000

    village monkey engine stub

---
+++ trunk/Village/src/kotucz/village/Multitexture.java
@@ -0,0 +1,39 @@
+package kotucz.village;
+
+/**
+ *
+ * @author Kotuc
+ */
+public class Multitexture {
+
+    int width;
+    int height;
+
+    public Multitexture(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Subtexture createSubtexture(int startX, int startY, int endX, int endY) {
+        return new Subtexture((float) startX / width,
+                (float) (height - endY) / height,
+                (float) endX / width,
+                (float) (height - startY) / height);
+    }
+}
