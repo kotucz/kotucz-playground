8ccea7669e9acb3dfe4369369432619bded413b7
TomasRKotula@gmail.com <TomasRKotula@gmail.com@ef30e6ac-8c31-e848-2268-f4fed5c5a1f9>
2012-06-06 22:08:04 +0000

    picking

--- Village/src/kotucz/village/game/MyGame.java
+++ Village/src/kotucz/village/game/MyGame.java
@@ -38,6 +38,8 @@ import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
 import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
@@ -46,11 +48,14 @@ import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.BlendMode;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.AbstractBox;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Sphere;
@@ -79,17 +84,22 @@ public class MyGame extends SimpleApplication {
     Material matgrass;
     Material matroad;
     Material matwtr;
+    Material matsel;
     BasicShadowRenderer bsr;
     private static Sphere bullet;
     private static MyBox box;
     private static Box brick;
     private static SphereCollisionShape bulletCollisionShape;
     private BulletAppState bulletAppState;
+    Geometry mark;
+    Node shootables;
+    
 
     public static void main(String args[]) {
         MyGame f = new MyGame();
         f.start();
     }
+    private RoadTileGrid selectTileGrid;
 
     @Override
     public void simpleInitApp() {
@@ -102,6 +112,9 @@ public class MyGame extends SimpleApplication {
         bullet.setTextureMode(TextureMode.Projected);
         bulletCollisionShape = new SphereCollisionShape(0.4f);
 
+        shootables = new Node("Shootables");
+        rootNode.attachChild(shootables);
+        
         Multitexture mtex = new Multitexture(16 * 4, 16 * 4);
 //        mtex.createSubtexture(0, 16, 16, 32);
 
@@ -122,6 +135,10 @@ public class MyGame extends SimpleApplication {
 //        initFloor();
         initGrid();
         initCrossHairs();
+
+        initKeys();       // load custom key mappings
+        initMark();       // a red sphere to mark the hit
+
         this.cam.setLocation(new Vector3f(8, -8, 8f));
         cam.lookAt(new Vector3f(8, 8, 0), UP);
         cam.setFrustumFar(50);
@@ -135,6 +152,19 @@ public class MyGame extends SimpleApplication {
         bsr.setDirection(new Vector3f(-1, -1, -1).normalizeLocal());
         viewPort.addProcessor(bsr);
     }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+    
+        
+       pick();
+    
+    }
+    
+    
+
+ 
 
     private PhysicsSpace getPhysicsSpace() {
         return bulletAppState.getPhysicsSpace();
@@ -159,8 +189,56 @@ public class MyGame extends SimpleApplication {
             if (name.equals("gc") && !keyPressed) {
                 System.gc();
             }
+            if (name.equals("Shoot") && !keyPressed) {
+                pick();
+            }
+
         }
     };
+
+    void pick() {
+        // 1. Reset results list.
+        CollisionResults results = new CollisionResults();
+        // 2. Aim the ray from cam loc to cam direction.
+        Ray ray = new Ray(cam.getLocation(), cam.getDirection());
+        // 3. Collect intersections between Ray and Shootables in results list.
+        shootables.collideWith(ray, results);
+        // 4. Print the results
+        System.out.println("----- Collisions? " + results.size() + "-----");
+        for (int i = 0; i < results.size(); i++) {
+            // For each hit, we know distance, impact point, name of geometry.
+            float dist = results.getCollision(i).getDistance();
+            Vector3f pt = results.getCollision(i).getContactPoint();
+            String hit = results.getCollision(i).getGeometry().getName();
+            System.out.println("* Collision #" + i);
+            System.out.println("  You shot " + hit + " at " + pt + ", " + dist + " wu away.");
+
+
+
+        }
+
+
+        // 5. Use the results (we mark the hit object)
+        if (results.size() > 0) {
+            // The closest collision point is what was truly hit:
+            CollisionResult closest = results.getClosestCollision();
+            // Let's interact - we mark the hit with a red dot.
+            mark.setLocalTranslation(closest.getContactPoint());
+            rootNode.attachChild(mark);
+
+            if ("selgrid".equals(closest.getGeometry().getName())) {
+                Vector3f contactPoint = closest.getContactPoint();
+                int x = (int) Math.floor(contactPoint.x);
+                int y = (int) Math.floor(contactPoint.y);
+                selectTileGrid.setTexture(x, y, 1);
+                selectTileGrid.updateGeometry();
+            }
+
+        } else {
+            // No hits? Then remove the red mark.
+            rootNode.detachChild(mark);
+        }
+    }
 
     public void initBoxes() {
 
@@ -221,6 +299,8 @@ public class MyGame extends SimpleApplication {
         {
 
             PathNetwork pnet = new PathNetwork(16, 16);
+            pnet.randomlySelect(40);
+
 
             final Geometry geometry = new Geometry("grid16", new NodedefTileGrid(pnet));
             geometry.setMaterial(matwtr);
@@ -232,6 +312,7 @@ public class MyGame extends SimpleApplication {
 
 
             PathNetwork pnet = new PathNetwork(16, 16);
+            pnet.randomlySelect(80);
 
             final Geometry geometry = new Geometry("roadgrid", new RoadTileGrid(pnet));
             geometry.setMaterial(matroad);
@@ -242,8 +323,22 @@ public class MyGame extends SimpleApplication {
         }
 
 
+        {
 
+
+            PathNetwork pnet = new PathNetwork(16, 16);
+            pnet.randomlySelect(20);
+            selectTileGrid = new RoadTileGrid(pnet);
+
+            selgeom = new Geometry("selgrid", selectTileGrid);
+            selgeom.setMaterial(matsel);
+            selgeom.setShadowMode(ShadowMode.Receive);
+            selgeom.setLocalTranslation(new Vector3f(0, 0, 0.0015f));
+            selgeom.setQueueBucket(Bucket.Transparent);
+            this.shootables.attachChild(selgeom);
+        }
     }
+    Geometry selgeom;
 
     public void initFloor() {
         Box floorBox = new Box(Vector3f.ZERO, 10f, 0.1f, 5f);
@@ -315,6 +410,18 @@ public class MyGame extends SimpleApplication {
             matroad.setTexture("ColorMap", tex4);
             matroad.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
         }
+        {
+            matsel = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+            TextureKey key4 = new TextureKey("Textures/select16.png");
+//            TextureKey key4 = new TextureKey("Textures/watr16.png");
+//            TextureKey key3 = new TextureKey("Textures/tex16.png");
+//            key3.setGenerateMips(true);`
+            Texture tex4 = assetManager.loadTexture(key4);
+            tex4.setMagFilter(Texture.MagFilter.Nearest);
+//        tex3.setWrap(WrapMode.Repeat);
+            matsel.setTexture("ColorMap", tex4);
+            matsel.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+        }
     }
 
     public void addBox(Vector3f ori, AbstractBox box) {
@@ -354,5 +461,22 @@ public class MyGame extends SimpleApplication {
                 settings.getWidth() / 2 - guiFont.getCharSet().getRenderedSize() / 3 * 2,
                 settings.getHeight() / 2 + ch.getLineHeight() / 2, 0);
         guiNode.attachChild(ch);
+    }
+
+    /** Declaring the "Shoot" action and mapping to its triggers. */
+    private void initKeys() {
+        inputManager.addMapping("Shoot",
+                new KeyTrigger(KeyInput.KEY_SPACE), // trigger 1: spacebar
+                new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); // trigger 2: left-button click
+        inputManager.addListener(actionListener, "Shoot");
+    }
+
+    /** A red ball that marks the last spot that was "hit" by the "shot". */
+    protected void initMark() {
+        Sphere sphere = new Sphere(30, 30, 0.2f);
+        mark = new Geometry("BOOM!", sphere);
+        Material mark_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mark_mat.setColor("Color", ColorRGBA.Red);
+        mark.setMaterial(mark_mat);
     }
 }
