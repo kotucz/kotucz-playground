b49a80e84c07406230d608854f244a3adcbb3396
TomasRKotula@gmail.com <TomasRKotula@gmail.com@ef30e6ac-8c31-e848-2268-f4fed5c5a1f9>
2012-06-09 16:15:59 +0000

    Building

--- Village/src/kotucz/village/build/Building.java
+++ Village/src/kotucz/village/build/Building.java
@@ -1,6 +1,10 @@
 package kotucz.village.build;
 
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import kotucz.village.MyBox;
 import kotucz.village.tiles.Pos;
 
 /**
@@ -11,13 +15,48 @@ public class Building {
 
     Pos pos;
 
-    public Building(Pos pos, Node rootNode) {
+    final Node node = new Node("Budova");
+    
+    public Building(Pos pos, Material mat16) {
         this.pos = pos;
-        
-        new Node();       
-        
+                                
+        {
+            MyBox box = new MyBox(Vector3f.ZERO, new Vector3f(1, 1, 1));
+            Geometry reBoxg = new Geometry("brick3", box);
+            reBoxg.setMaterial(mat16);
+            reBoxg.setLocalTranslation(new Vector3f(0, 0, 0));
+
+            node.attachChild(reBoxg);
+        }
+    
+        {
+            MyBox box = new MyBox(Vector3f.ZERO, new Vector3f(1, 1, 1));
+            Geometry reBoxg = new Geometry("brick3", box);
+            reBoxg.setMaterial(mat16);
+            reBoxg.setLocalTranslation(new Vector3f(1, 0, 0));
+           
+
+            node.attachChild(reBoxg);
+        }
+        {
+            MyBox box = new MyBox(Vector3f.ZERO, new Vector3f(1, 1, 1));
+            Geometry reBoxg = new Geometry("brick3", box);
+            reBoxg.setMaterial(mat16);
+            reBoxg.setLocalTranslation(new Vector3f(0, 1, 0));
+           
+
+            node.attachChild(reBoxg);
+        }
+    
+        node.setLocalTranslation(pos.x, pos.y, 0);
         
     }
+
+    public Node getNode() {
+        return node;
+    }
+    
+    
     
     
     
