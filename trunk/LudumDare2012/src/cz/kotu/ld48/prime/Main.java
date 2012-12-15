a00a7e9c26adf5a417250161df358d00c1697443
TomasRKotula@gmail.com <TomasRKotula@gmail.com@ef30e6ac-8c31-e848-2268-f4fed5c5a1f9>
2012-12-15 19:04:57 +0000

    ludum dare 2012 Theme: You are The Villain

---
+++ trunk/LudumDare2012/src/cz/kotu/ld48/prime/Main.java
@@ -0,0 +1,25 @@
+package cz.kotu.ld48.prime;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame jFrame = new JFrame("Get Away!!!");
+        final GamePanel comp = new GamePanel();
+        jFrame.add(comp);
+        jFrame.pack();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+
+
+        new Thread(){
+            public void run(){
+                comp.game.loop();
+            }
+        }.start();
+
+
+    }
+}
