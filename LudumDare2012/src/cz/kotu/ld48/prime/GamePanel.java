30ab3a8e44845573cebd561b18cdc8a74f0fa314
TomasRKotula@gmail.com <TomasRKotula@gmail.com@ef30e6ac-8c31-e848-2268-f4fed5c5a1f9>
2012-12-15 20:21:25 +0000

    first sound effect

--- LudumDare2012/src/cz/kotu/ld48/prime/GamePanel.java
+++ LudumDare2012/src/cz/kotu/ld48/prime/GamePanel.java
@@ -1,12 +1,14 @@
 package cz.kotu.ld48.prime;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.EnumSet;
 
@@ -16,6 +18,8 @@ import java.util.EnumSet;
 public class GamePanel extends JPanel {
 
     private Image villain;
+    private GamePanel.Sound crash;
+
 
     enum Key {
         ELSE,
@@ -30,7 +34,6 @@ public class GamePanel extends JPanel {
 
     final Game game = new Game(this
     );
-
 
 
     public GamePanel() {
@@ -49,7 +52,7 @@ public class GamePanel extends JPanel {
         addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                crash.play();
             }
 
             @Override
@@ -68,7 +71,7 @@ public class GamePanel extends JPanel {
     }
 
     private Key toKey(KeyEvent e) {
-        switch(e.getKeyCode()) {
+        switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
             case KeyEvent.VK_UP:
             case KeyEvent.VK_KP_UP:
@@ -95,19 +98,16 @@ public class GamePanel extends JPanel {
 
 //        System.out.println("" + width + " x " + height);
 
-        g.drawImage(villain, (int)(game.x%width), (int)(game.y%height), null);
+        g.drawImage(villain, (int) (game.x % width), (int) (game.y % height), null);
 
         g.draw(new Rectangle2D.Double(2, 2, width - 4, height - 4));
 
-        g.draw(new Rectangle2D.Double(game.x%width, game.y%height, 20, 20));
+        g.draw(new Rectangle2D.Double(game.x % width, game.y % height, 20, 20));
 
 
-
-        g.drawString((downKeys.contains(Key.UP)?"UP":"up"), 50 , 50);
-        g.drawString((downKeys.contains(Key.DOWN)?"DOWN":"down"), 50 , 75);
-        g.drawString((downKeys.contains(Key.ELSE)?"ELSE":"else"), 50 , 100);
-
-
+        g.drawString((downKeys.contains(Key.UP) ? "UP" : "up"), 50, 50);
+        g.drawString((downKeys.contains(Key.DOWN) ? "DOWN" : "down"), 50, 75);
+        g.drawString((downKeys.contains(Key.ELSE) ? "ELSE" : "else"), 50, 100);
 
 
     }
@@ -115,14 +115,62 @@ public class GamePanel extends JPanel {
     void loadImages() {
 
         try {
-            villain = ImageIO.read(getClass().getResourceAsStream("/villain.png"));
-
+            villain = loadImage("/villain.png");
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
     }
 
+    private BufferedImage loadImage(String name) throws IOException {
+        return ImageIO.read(getClass().getResourceAsStream(name));
+    }
+
+    void loadSounds() {
+
+
+        try {
+            crash = loadSound("/sounds/crash.wav");
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+    }
+
+    private Sound loadSound(String name) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        AudioInputStream audioInputStream = null;
+        audioInputStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream(name));
+
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+        return new Sound(clip);
+
+    }
+
+
+    class Sound{
+        private Clip clip;
+
+        public Sound(Clip clip) {
+            this.clip = clip;
+        }
+
+        void play() {
+            // TODO volume mute
+//            if (volume != Volume.MUTE) {
+                if (clip.isRunning())
+                    clip.stop();   // Stop the player if it is still running
+                clip.setFramePosition(0); // rewind to the beginning
+                clip.start();     // Start playing
+//            }
+        }
+    }
 
 
 }
