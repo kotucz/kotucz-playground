e67c7ad834c36f0d351e9854ffc1bc01b11538d9
TomasRKotula@gmail.com <TomasRKotula@gmail.com@ef30e6ac-8c31-e848-2268-f4fed5c5a1f9>
2012-12-16 13:06:56 +0000

    centering - refactored colisions

--- trunk/LudumDare2012/src/cz/kotu/ld48/prime/Entity.java
+++ trunk/LudumDare2012/src/cz/kotu/ld48/prime/Entity.java
@@ -1,6 +1,7 @@
 package cz.kotu.ld48.prime;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 
 /**
@@ -10,9 +11,37 @@ public class Entity {
 
     Image image;
     final Rectangle2D.Double rect;
+    double scale =
+            Game.INV_PIXELS_PER_METER;
 
     public Entity(Image image, double x, double y, double w, double h) {
         this.image = image;
         rect = new Rectangle2D.Double(x, y, w, h);
+
+
+
     }
+
+    void draw(Graphics2D g) {
+        AffineTransform tf = new AffineTransform();
+        tf.translate(rect.x, rect.y);
+        tf.scale(scale, scale);
+        g.drawImage(image, tf, null);
+        g.draw(rect);
+    }
+
+    void drawCentered(Graphics2D g) {
+        drawCentered(g, 0);
+    }
+
+    void drawCentered(Graphics2D g, double rot) {
+        AffineTransform tf = new AffineTransform();
+        tf.translate(rect.getCenterX(), rect.getCenterY());
+        tf.scale(scale, scale);
+        tf.rotate(rot);
+        tf.translate(-image.getWidth(null)/2, -image.getHeight(null)/2);
+        g.drawImage(image, tf, null);
+        g.draw(rect);
+    }
+
 }
